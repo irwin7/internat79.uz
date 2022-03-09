7272dcad9c9b7605489f856409df86ffaa07a886
irwin7 <ravshankhan777@icloud.com>
2022-03-10 00:23:07 +0500

    kinderg done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const elMenuBtn = document.querySelector('.header__btn');
 const elHeader = document.querySelector('.header');
+const elMain = document.querySelector('#main');
+const elBtnPrev = document.querySelector('.slick-prev');
+const elBtnNext = document.querySelector('.slick-next');
 
 elMenuBtn.addEventListener('click',() => {
   elHeader.classList.toggle('header--active');
@@ -7,6 +10,14 @@ elMenuBtn.addEventListener('click',() => {
 
 elHeader.addEventListener('blur',() => {
   elHeader.classList.remove('header--active');
+})
+
+window.addEventListener("scroll",() => {
+  if(window.scrollY >= 100){
+    elMain.classList.add("scrolled");
+  }else{
+    elMain.classList.remove("scrolled");
+  }
 })
 
 $('.wrapper__list').slick({
