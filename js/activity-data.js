ef0cc5320e55d35c9c2b53a8b11fc592b82195e2
irwin7 <ravshankhan777@icloud.com>
2022-03-11 19:25:28 +0500

    activities done

---
+++ js/activity-data.js
@@ -0,0 +1,62 @@
+const activityData = [
+  {
+    type: "music",
+    title: "music",
+    text: "One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, iste, architecto ullam tenetur quia nemo ratione tempora consectetur quos minus ut quo nulla ipsa aliquid molestias.",
+    staticText: "One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, iste, architecto ullam tenetur quia nemo ratione tempora consectetur quos minus ut quo nulla ipsa aliquid neque molestias et qui sunt. Odit, molestiae.",
+    advantages: [
+      "lorem",
+      "lorem",
+      "lorem"
+    ],
+    img: "./img/activities/music.jpg"
+  },
+  {
+    type: "learn",
+    title: "learn",
+    text: "One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, iste, architecto ullam tenetur quia nemo ratione tempora consectetur quos minus ut quo nulla ipsa aliquid molestias.",
+    staticText: "One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, iste, architecto ullam tenetur quia nemo ratione tempora consectetur quos minus ut quo nulla ipsa aliquid neque molestias et qui sunt. Odit, molestiae.",
+    advantages: [
+      "lorem",
+      "lorem",
+      "lorem"
+    ],
+    img: "./img/activities/learning.jpg"
+  },
+  {
+    type: "painting",
+    title: "painting",
+    text: "One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, iste, architecto ullam tenetur quia nemo ratione tempora consectetur quos minus ut quo nulla ipsa aliquid molestias.",
+    staticText: "One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, iste, architecto ullam tenetur quia nemo ratione tempora consectetur quos minus ut quo nulla ipsa aliquid neque molestias et qui sunt. Odit, molestiae.",
+    advantages: [
+      "lorem",
+      "lorem",
+      "lorem"
+    ],
+    img: "./img/activities/painting.jpg"
+  },
+  {
+    type: "sport",
+    title: "sport",
+    text: "One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, iste, architecto ullam tenetur quia nemo ratione tempora consectetur quos minus ut quo nulla ipsa aliquid molestias.",
+    staticText: "One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, iste, architecto ullam tenetur quia nemo ratione tempora consectetur quos minus ut quo nulla ipsa aliquid neque molestias et qui sunt. Odit, molestiae.",
+    advantages: [
+      "lorem",
+      "lorem",
+      "lorem"
+    ],
+    img: "./img/activities/sport.jpg"
+  },
+  {
+    type: "playground",
+    title: "playground",
+    text: "One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, iste, architecto ullam tenetur quia nemo ratione tempora consectetur quos minus ut quo nulla ipsa aliquid molestias.",
+    staticText: "One Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, iste, architecto ullam tenetur quia nemo ratione tempora consectetur quos minus ut quo nulla ipsa aliquid neque molestias et qui sunt. Odit, molestiae.",
+    advantages: [
+      "lorem",
+      "lorem",
+      "lorem"
+    ],
+    img: "./img/activities/playground.jpg"
+  },
+]
--- js/main.js
+++ js/main.js
@@ -1,28 +1,82 @@
-const elMenuBtn = document.querySelector('.header__btn');
-const elHeader = document.querySelector('.header');
-const elMain = document.querySelector('#main');
-const elBtnPrev = document.querySelector('.slick-prev');
-const elBtnNext = document.querySelector('.slick-next');
+const elMenuBtn = document.querySelector(".header__btn");
+const elHeader = document.querySelector(".header");
+const elMain = document.querySelector("#main");
+const elBtnPrev = document.querySelector(".slick-prev");
+const elBtnNext = document.querySelector(".slick-next");
+const elActivities = document.querySelectorAll(".activities__control");
+const elActivity = document.querySelector(".activity");
+const template = document.querySelector('#activityTemplate');
 
-elMenuBtn.addEventListener('click',() => {
-  elHeader.classList.toggle('header--active');
-})
+addItemHandler(template,activityData[0])
 
-elHeader.addEventListener('blur',() => {
-  elHeader.classList.remove('header--active');
-})
+function removeActiveHandler(items,className) {
+  for(let item of items){
+    item.classList.remove(className);
+  }
+}
 
-window.addEventListener("scroll",() => {
-  if(window.scrollY >= 100){
+function addItemHandler(templ,currentData) {
+  const clone = templ.content.cloneNode(true);
+  const title = clone.querySelector('.activity__title');
+  const text = clone.querySelector('.activity__text');
+  const staticText = clone.querySelector('.activity__static-text');
+  const advantages = clone.querySelectorAll('.activity__inner-text');
+  const activityImg = clone.querySelector('.activity__img');
+  
+  title.textContent = currentData.title;
+  text.textContent = currentData.text;
+  staticText.textContent = currentData.staticText;
+  activityImg.src = currentData.img;
+  for(let i = 0; i < currentData.advantages.length; i++){
+    advantages[i].textContent = currentData.advantages[i];
+  }
+
+  elActivity.innerHTML = "";
+  elActivity.appendChild(clone)
+}
+
+elMenuBtn.addEventListener("click", () => {
+  elHeader.classList.toggle("header--active");
+});
+
+elHeader.addEventListener("blur", () => {
+  elHeader.classList.remove("header--active");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 100) {
     elMain.classList.add("scrolled");
-  }else{
+  } else {
     elMain.classList.remove("scrolled");
   }
-})
+});
 
-$('.wrapper__list').slick({
+for(let i = 0; i < elActivities.length; i++) {
+  elActivities[i].addEventListener("click", () => {
+    const actValue = elActivities[i].attributes.activitytype.nodeValue;
+    const currentData = activityData.filter((item) => item.type === actValue);
+    addItemHandler(template,currentData[0]);
+    removeActiveHandler(elActivities,'activities__control--active');
+    elActivities[i].classList.add('activities__control--active');
+  });
+}
+
+$(".wrapper__list").slick({
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: true
+  arrows: true,
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
